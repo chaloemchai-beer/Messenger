415066dcb12af027059d4c185db05f9fe96704fc
Chaloemchai Aintharaphithak <ChaleamchaiSt@gmail.com>
2021-05-24 12:34:52 +0700

    messenger

--- src/firebase.js
+++ src/firebase.js
@@ -1,7 +1,13 @@
 import firebase from 'firebase'
 
 const firebaseApp = firebase.initializeApp({
-    // your Firebase credentials go here
+    apiKey: "AIzaSyAu_bMRFfqEJd3x7H67zxoApDo6UfMc3PE",
+    authDomain: "messenger-7f6c5.firebaseapp.com",
+    projectId: "messenger-7f6c5",
+    storageBucket: "messenger-7f6c5.appspot.com",
+    messagingSenderId: "419927064169",
+    appId: "1:419927064169:web:515a1f2e22a1be0ea12cf8",
+    measurementId: "G-LMDL4XH8SJ"
 })
 
 const db = firebaseApp.firestore()
